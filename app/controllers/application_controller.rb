99a6ba06318b14cf13f555d7dd9f362404477d5a
Paul McKellar <paul.mckellar@gmail.com>
2011-11-23 15:41:01 -0800

    fix app domain redirect issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
 
   before_filter :ensure_domain
 
-  APP_DOMAIN = 'http://udidirectory.com/'
+  APP_DOMAIN = 'udidirectory.com'
 
 protected
 
   def ensure_domain
     return unless Rails.env.production?
     if request.env['HTTP_HOST'] != APP_DOMAIN
-      redirect_to APP_DOMAIN, :status => 301
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
     end
   end
 end
