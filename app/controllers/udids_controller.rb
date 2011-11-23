09441541409f792d195a8e44b5e963c2864394cf
Paul McKellar <paul.mckellar@gmail.com>
2011-11-23 14:02:30 -0800

    only show you your udids

--- app/controllers/udids_controller.rb
+++ app/controllers/udids_controller.rb
@@ -2,23 +2,23 @@ class UdidsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @udids = Udid.all
+    @udids = current_user.udids.all
   end
 
   def show
-    @udid = Udid.find(params[:id])
+    @udid = current_user.udids.find(params[:id])
   end
 
   def new
-    @udid = Udid.new
+    @udid = current_user.udids.new
   end
 
   def edit
-    @udid = Udid.find(params[:id])
+    @udid = current_user.udids.find(params[:id])
   end
 
   def create
-    @udid = Udid.new(params[:udid])
+    @udid = current_user.udids.new(params[:udid])
 
     if @udid.save
       redirect_to @udid, :notice => 'Udid was successfully created.'
@@ -28,7 +28,7 @@ class UdidsController < ApplicationController
   end
 
   def update
-    @udid = Udid.find(params[:id])
+    @udid = current_user.udids.find(params[:id])
 
     if @udid.update_attributes(params[:udid])
       redirect_to @udid, :notice => 'Udid was successfully updated.'
@@ -38,7 +38,7 @@ class UdidsController < ApplicationController
   end
 
   def destroy
-    @udid = Udid.find(params[:id])
+    @udid = current_user.udids.find(params[:id])
     @udid.destroy
     redirect_to udids_url
   end
