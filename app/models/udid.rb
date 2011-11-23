91ca0adff794c95847d911d14bf9bcdceab0547f
Paul McKellar <paul.mckellar@gmail.com>
2011-11-23 15:07:46 -0800

    more specific validations for udids

--- app/models/udid.rb
+++ app/models/udid.rb
@@ -1,3 +1,13 @@
 class Udid < ActiveRecord::Base
   belongs_to :user
+
+  before_validation :strip_udid
+  validates :user, :presence => true
+  validates_length_of :udid, :is => 40, :message => "must be 40 characters"
+
+private
+
+  def strip_udid
+    self.udid = self.udid.gsub(/[^A-za-z0-9]+/,'')
+  end
 end
