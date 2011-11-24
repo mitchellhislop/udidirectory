42134380a13e31d4f29674a7efd9339869e87876
Paul McKellar <paul.mckellar@gmail.com>
2011-11-23 18:54:14 -0800

    can see udids by username

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -1,5 +1,6 @@
 class TwitterUsersController < ApplicationController
   def show
+    @user = TwitterUser.find_by_param(params["id"])
   end
 
 end
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,3 +1,11 @@
 class TwitterUser < ActiveRecord::Base
   has_many :udids
+
+  def self.find_by_param(screen_name)
+    self.find_by_screen_name(screen_name)
+  end
+
+  def to_param
+    self.screen_name
+  end
 end
