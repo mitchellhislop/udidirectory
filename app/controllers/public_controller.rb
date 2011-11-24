51a8a638821fcad0b3ab09ef05c2c6117a9189b0
Patrick B. Gibson <patrick@fadeover.org>
2011-11-24 02:20:15 -0800

    Add a privacy and safety page.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,4 +2,7 @@ class PublicController < ApplicationController
   def index
     redirect_to udids_url if current_user
   end
+  
+  def privacy_and_safety
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Udidirectory::Application.routes.draw do
   get '/profile', :to => 'session#show', :as => 'show'
   get '/signout', :to => 'session#destroy', :as => 'signout'
 
+  get '/privacyandsafety', :to => 'public#privacy_and_safety'
+
   root :to => 'public#index'
 end
