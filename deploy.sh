28adf9f8083c04157409cb2206d36c6c1cc3045b
Paul McKellar <paul.mckellar@gmail.com>
2011-11-23 19:39:08 -0800

    push to github head

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ RAILS_ENV=production bundle exec rake assets:precompile &&
   git add public/assets &&
   git commit -m "vendor compiled assets" &&
   git push heroku master
+  git push origin master
