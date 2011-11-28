d58cf5ed26f2d6a893fbc77ca156d9f2f33f4009
Patrick B. Gibson <patrick@fadeover.org>
2011-11-27 17:39:03 -0800

    Adding a new header and search field to pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+  def header_for_page(title = "", show_search = true)
+    code = <<-END_HTML
+    <div class="row">
+      <div class="span-two-thirds">
+        <div class="page-header">
+      	  <h1>UDIDirectory <small>#{title}</small></h1>
+      	</div>
+      </div>
+      <div class="span-one-third">
+    END_HTML
+    
+    if show_search then
+      code << capture do
+        form_for :twitter_user, :url => search_url do |f|
+          concat raw "<fieldset>"
+          concat text_field_tag :q
+          concat raw "&nbsp;&nbsp;"
+          concat submit_tag "Search", :class => "btn"
+          concat raw "</fieldset>"
+        end
+      end
+    end
+    
+    # End the span and row.
+    code << "</div></div>"    
+    return code
+  end
+
 end
