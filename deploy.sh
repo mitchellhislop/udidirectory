c20c93eaecb8ae74694260f4fbd3c46f1379c4a1
Patrick B. Gibson <patrick@fadeover.org>
2011-11-28 17:08:01 -0800

    Update heroku only after we update github.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 RAILS_ENV=production bundle exec rake assets:precompile &&
   git add public/assets &&
   git commit -m "vendor compiled assets" &&
-  git push heroku master
   git push origin master
+  git push heroku master
